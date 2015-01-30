bafacd04276972aeb1e12a1662155d98c73cadc0
hepa <szabolcs.remenyi@gmail.com>
2015-01-30 22:11:33 +0100

    IModule.cs added.

---
+++ ExtensibilityDLL/Modules/IModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExtensibilityDLL.Modules
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IModule
+    {
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Gets the icon's URL of the module. 
+        /// </summary>
+        string Icon { get; }
+
+        /// <summary>
+        /// Gets the developer of the module.
+        /// </summary>
+        string Developer { get; }
+
+        /// <summary>
+        /// Gets the version of the module.
+        /// </summary>
+        Version Version { get; }
+    }
+}
